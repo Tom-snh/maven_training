c85304701c44168b0432843d69c834013f7f814e
Tom-snh <tsinnah@hotmail.com>
2021-03-10 18:35:41 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,26 @@
+package fr.lernejo;
+
+    import org.junit.jupiter.api.Test;
+    import java.util.function.BiFunction;
+    import static org.junit.jupiter.api.Assertions.*;
+
+    import org.junit.jupiter.params.ParameterizedTest;
+    import org.junit.jupiter.params.provider.CsvSource;
+    import static  org.assertj.core.api.Assertions.assertThat;
+
+
+class SampleTest {
+    private final Sample sample = new Sample();
+
+    @ParameterizedTest
+    @CsvSource({
+        "3,7,10",
+        "0,3,3",
+        "4,5,9",
+    })
+    void add_behaves_as_expected(int a, int b, int expectedResult){
+        int result = sample.op(Sample.Operation.ADD,a,b);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
